d1661aff4711c2fcc212a85dfee9e7991c2df1e5
Daniel Bong <bongster88@gmail.com>
2021-07-01 07:32:29 +0000

    Feature/crud

---
+++ src/api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"droneia-go/src/api/controller"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/lib/pq"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	_, err := controller.NewDB("postgres://droneina:droneina@192.168.1.9:15432/droneina")
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("DB Connected")
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello)
+	e.GET("/TaskGroups", controller.GetTaskGroups)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello world")
+}
