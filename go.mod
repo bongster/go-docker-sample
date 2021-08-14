c2fb00f3b0fc8bc9d1cc067224c9f980ef6c2cde
Daniel <bongster88@gmail.com>
2021-08-14 18:24:05 +0800

    implement GET, POST chat api

--- go.mod
+++ go.mod
@@ -4,8 +4,16 @@ go 1.16
 
 require (
 	github.com/360EntSecGroup-Skylar/excelize/v2 v2.4.0
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/labstack/echo/v4 v4.3.0
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.2
+	github.com/stretchr/testify v1.7.0 // indirect
+	go.mongodb.org/mongo-driver v1.7.1
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- src/api/controller/db.go
+++ src/api/controller/db.go
@@ -1,8 +1,14 @@
 package controller
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func NewDB(dataSourceName string) (*sql.DB, error) {
@@ -14,4 +20,22 @@ func NewDB(dataSourceName string) (*sql.DB, error) {
 		return nil, err1
 	}
 	return db, nil
+}
+
+func NewMongoDB(dataSourceName string) (*mongo.Client, error) {
+	clientOpts := options.Client().ApplyURI(dataSourceName)
+	client, err := mongo.NewClient(clientOpts)
+	if err != nil {
+		return nil, err
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
 }
