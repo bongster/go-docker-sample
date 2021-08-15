0716dbb5559b228ba808963c8ce095a8044ecba6
Sungju bong <bongster88@gmail.com>
2021-08-15 21:20:57 +0800

    create message with chat relationship

--- src/api/controller/ChatCtrl.go
+++ src/api/controller/ChatCtrl.go
@@ -8,6 +8,8 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	model "droneia-go/src/api/model"
@@ -80,4 +82,60 @@ func UpdateChat(c echo.Context) error {
 func DeleteChat(c echo.Context) error {
 	dummy := new(interface{})
 	return c.JSON(http.StatusNotImplemented, dummy)
+}
+
+func GetChatMessages(c echo.Context) error {
+	id, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		return err
+	}
+	client, err := NewMongoDB("mongodb://admin:admin@mongo:27017")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+	collection := client.Database("app").Collection("messages")
+	var results []*model.Message
+	filterStage := bson.D{primitive.E{Key: "$limit", Value: 10}}
+	matchStage := bson.D{primitive.E{Key: "$match", Value: bson.D{primitive.E{Key: "chat", Value: id}}}}
+
+	cur, err := collection.Aggregate(context.TODO(), mongo.Pipeline{matchStage, filterStage})
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	if err = cur.All(context.TODO(), &results); err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	defer cur.Close(context.TODO())
+
+	return c.JSON(http.StatusOK, results)
+}
+
+func CreateChatMessages(c echo.Context) error {
+	id := c.Param("id")
+	client, err := NewMongoDB("mongodb://admin:admin@mongo:27017")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err)
+	}
+
+	model := new(model.Message)
+	model.Chat, err = primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+	if err = c.Bind(model); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if err = c.Validate(model); err != nil {
+		return err
+	}
+	collection := client.Database("app").Collection("messages")
+	insertResult, err := collection.InsertOne(context.TODO(), model)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return c.JSON(http.StatusCreated, insertResult)
 }
