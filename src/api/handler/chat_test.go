b50f4c4827ce3424709ec4ded972a9e77543d287
Sungju bong <bongster88@gmail.com>
2021-08-16 17:13:15 +0800

    add chat_test.go file

---
+++ src/api/handler/chat_test.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"droneia-go/src/api/db"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	chatJSON = `[{"name":"Jon Snow"}]`
+)
+
+func TestGetChat(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(echo.GET, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/chats")
+
+	db, _ := db.NewMongoDB(os.Getenv("MONGO_DB_URL"))
+	h := &Handler{
+		DB: db,
+	}
+
+	if assert.NoError(t, h.GetChat(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, chatJSON, rec.Body.String())
+	}
+}
